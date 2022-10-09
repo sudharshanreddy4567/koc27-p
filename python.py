513f98cb596e4d31d534a6a65bc636a269853798
sudharshanreddy4567 <115392665+sudharshanreddy4567@users.noreply.github.com>
2022-10-09 12:57:28 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+a = int(input("Please enter number 1 "))
+b = int(input("Please enter number 2 "))
+
+print("Addition is ", a+b)
+print("Subtraction is ", a-b)
+print("Multiplication is ", a*b)
+print("Division is ", a/b)
+print("Exponentiation is ", a**b)
+print("Floor division is ", a // b)
+print("Modulus is ", a%b)
